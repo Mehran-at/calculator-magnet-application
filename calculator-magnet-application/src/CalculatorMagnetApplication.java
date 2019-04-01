7af431dfc5914eafb5a5400e19e83214759f6d9e
Mehran Hosseini <ajaxcube@gmail.com>
2019-04-01 17:12:45 -0400

    adding the question as comment

--- calculator-magnet-application/src/CalculatorMagnetApplication.java
+++ calculator-magnet-application/src/CalculatorMagnetApplication.java
@@ -1,3 +1,9 @@
+//Derek wants to play around with his new programming abilities. He’s decided to create his own calculator magnet to place it on his fridge’s door.
+//Develop a code that asks for two numbers and says their sum.
+
+//Derek has decided to improve his magnet calculator with an action that can find out the square (power two) of a given number.
+//      a. Define an action that calculates the square (power two) of a given number.
+//      b. Develop a code that asks for two numbers, uses the previous action and says the result.
 import java.util.Scanner;
 
 public class CalculatorMagnetApplication {
