2ae13b141fdd98311a358d9f2eba2aaa6c23a661
Mehran Hosseini <ajaxcube@gmail.com>
2019-04-01 17:08:12 -0400

    adding square class

--- calculator-magnet-application/src/CalculatorMagnetApplication.java
+++ calculator-magnet-application/src/CalculatorMagnetApplication.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class CalculatorMagnetApplication {
     public static void main(String[] args) {
         Manager manager = new Manager();
+        Square square = new Square();
         Scanner scanner = new Scanner(System.in);
         int one = scanner.nextInt();
         int two = scanner.nextInt();
-        System.out.println(manager.getSum(one, two));
-
+        int sum = manager.getSum(one, two);
+        System.out.println("The sum of your two numbers is: " + sum);
+        System.out.println("And the square of that number is: " + square.getSquare(sum));
     }
 }
---
+++ calculator-magnet-application/src/Square.java
@@ -0,0 +1,7 @@
+public class Square {
+    Manager manager = new Manager();
+    public int getSquare(int sum) {
+        int square = sum * 2;
+        return square;
+    }
+}
